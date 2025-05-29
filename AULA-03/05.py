3756f8217298b171858e58724fc8f55fc88cd071
bNDorneles <bernardogomesdorneles13@gmail.com>
2025-05-29 01:27:41 -0300

    05e06

--- AULA-03/05.py
+++ AULA-03/05.py
@@ -1,12 +1,22 @@
+def calcular_aumento(salario):
+    if salario <= 1000:
+        aumento = salario * 0.20
+    elif 1000.01 <= salario <= 2000:
+        aumento = salario * 0.15
+    elif 2000.01 <= salario <= 3000:
+        aumento = salario * 0.10
+    else:
+        aumento = salario * 0.05
+    return aumento
 
+# Entrada do salário
+salario = float(input("Digite o salário do funcionário: R$ "))
 
-salario = float(input("Digite o salario: "))
-aumento = int(input("Digite o aumento: "))
+# Cálculo do aumento
+aumento = calcular_aumento(salario)
+novo_salario = salario + aumento
 
-valor_aumento = salario * (aumento/100)
-valor_final = salario + valor_aumento
-
-
-print(f"Valor dos itens: {salario}")
-print(f"Valor da gorjeta ({aumento}%): R$ {valor_aumento}")
-print(f"Valor total a pagar: {valor_final}")
+# Saída dos resultados
+print(f"Salário atual: R$ {salario:.2f}")
+print(f"Valor do aumento: R$ {aumento:.2f}")
+print(f"Novo salário: R$ {novo_salario:.2f}")
--- AULA-03/06.py
+++ AULA-03/06.py
@@ -1,3 +1,22 @@
+altura =  float(input("Digite a sua altura: "))
+peso = float(input("Digite seu peso: "))
 
-nome = "Bernardo"
-print(nome)
+imc = peso/(altura * altura)
+
+if imc < 18.5:
+    print(f"A seu imc é: {imc:.2f}, você está abaixo do peso!")
+
+elif imc >= 18.5 and imc <= 24.9:
+    print(f"A seu imc é: {imc:.2f}, você está no peso normal!")
+
+elif imc >= 25 and imc <= 29.9:
+    print(f"A seu imc é: {imc:.2f}, você está acima do peso!")
+
+elif imc >= 30 and imc <= 34.9:
+    print(f"A seu imc é: {imc:.2f}, você está com obsidade grau 1!")
+
+elif imc >= 35 and imc <= 39.9:
+    print(f"A seu imc é: {imc:.2f}, você está com obsidade grau 2!")
+
+elif imc >= 40:
+    print(f"A seu imc é: {imc:.2f}, você está com obsidade grau 3!")

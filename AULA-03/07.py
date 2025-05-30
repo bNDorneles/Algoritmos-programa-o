9e9a589b19d675ee2f2b5e6334dcf32a887bacfc
bNDorneles <bernardogomesdorneles13@gmail.com>
2025-05-30 00:59:12 -0300

    07feito

--- AULA-03/07.py
+++ AULA-03/07.py
@@ -1,16 +1,16 @@
 produto =  float(input("Digite o valor do produto: "))
 quantidade = float(input("Digite a quantidade de produtos: "))
 
-total_sem_desconto = produto * quantidade
+total_sem_desconto = produto * quantidade # Calcula valor sem desconto.
 
-if quantidade <= 10:
+if quantidade <= 10: 
    desconto = 0
 elif 11 <= quantidade <=20:
    desconto = total_sem_desconto * 0.10
 else:
-   desconto = total_sem_desconto * 0.20
+   desconto = total_sem_desconto * 0.20 # se quantidade maior que 20 
 
-total_com_desconto = total_sem_desconto - desconto
+total_com_desconto = total_sem_desconto - desconto  # Calcula valor com desconto final.
 
 print(f"Valor sem desconto: R$ {total_sem_desconto:.2f}")
 print(f"Desconto aplicado: R$ {desconto:.2f}")
--- AULA-03/08.py
+++
@@ -1,2 +0,0 @@
-nome = "Bernardo"
-print(nome)
